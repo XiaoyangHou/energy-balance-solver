bd9802aa5997575f6a28d5fada33ba0cee1fff24
XiaoyangHou <89182331+XiaoyangHou@users.noreply.github.com>
2022-06-14 15:06:37 +0800

    Update solver.cpp
    enlarge input matrix size

--- solver.cpp
+++ solver.cpp
@@ -14,7 +14,7 @@ struct Constraint{
     int32_t sum;
 };
 
-void output_blank_file(void); // 10 * 10 matrix
+void output_blank_file(void); // 12 * 12 matrix
 
 void write_solution(const vector<vector<string>> &matrix);
 
@@ -68,7 +68,12 @@ void init() {
     available_values_lock.resize(number_of_available_value, 0);
 
     get_candidate_values_and_constraints(matrix, candidate_value_coordinates, candidates_map, constraints_vct);
-    // output_matrix(candidates_map);
+//    output_matrix(candidates_map);
+//    for (const auto &p : constraints_vct) {
+//        cout << p.sum << " = ";
+//        for (const auto &e : p.variable_id) cout << e << " ";
+//        cout << endl;
+//    }
     number_of_candidate_value = candidate_value_coordinates.size() - 1;
     assert(number_of_candidate_value == number_of_available_value);
     dfs_values.resize(number_of_candidate_value);
@@ -120,15 +125,15 @@ bool solve() {
 }
 
 void output() {
-    vector<vector<string>> out_matrix(10, vector<string> (10));
+    vector<vector<string>> out_matrix(12, vector<string> (12));
     for (uint32_t index = 0; index < number_of_candidate_value; index++) {
         uint32_t id = dfs_order[index];
         uint32_t x, y;
         tie(x, y) = candidate_value_coordinates[id];
         out_matrix[x][y] = to_string(dfs_values[index]);
     }
-    for (uint32_t i = 0; i < 10; i++) {
-        for (uint32_t j = 0; j < 10; j++) {
+    for (uint32_t i = 0; i < 12; i++) {
+        for (uint32_t j = 0; j < 12; j++) {
             if (isalpha(matrix[i][j].back())) {
                 out_matrix[i][j] = matrix[i][j];
             }
@@ -179,14 +184,14 @@ int main() {
     return 0;
 }
 
-void output_blank_file() { // 10 * 10 matrix
+void output_blank_file() { // 12 * 12 matrix
     ofstream out("input_file.txt", ios::out);
     assert(out.is_open());
-    for (uint32_t i = 0; i < 10; i++) {
-        for (uint32_t j = 1; j < 10; j++) {
-            out << "    ,";
+    for (uint32_t i = 0; i < 12; i++) {
+        for (uint32_t j = 1; j < 12; j++) {
+            out << "      ,";
         }
-        out << "    " << endl;
+        out << "      " << endl;
     }
     out.close();
     out.clear();
@@ -205,7 +210,7 @@ vector<string> split_by_dot(const string &str) {
         }
     }
     ret.emplace_back(tmp);
-    assert(ret.size() == 10); // assure 10 column each row
+    assert(ret.size() == 12); // assure 12 column each row
     return ret;
 }
 
@@ -216,21 +221,23 @@ vector<vector<string>> read_from_txt() {
     while (true) {
         string tmp;
         getline(in, tmp);
+        while (tmp.back() == '\r' || tmp.back() == '\n')
+            tmp.pop_back();
         if (tmp.empty()) break;
         ret.emplace_back(split_by_dot(tmp));
     }
     in.close();
     in.clear();
-    assert(ret.size() == 10); // assure that 10 row
+    assert(ret.size() == 12); // assure that 12 row
     return ret;
 }
 
 void write_solution(const vector<vector<string>> &matrix) {
     ofstream out("solution.txt", ios::out);
-    for (uint32_t i = 0; i < 10; i++) {
-        for (uint32_t j = 0; j < 10; j++) {
+    for (uint32_t i = 0; i < 12; i++) {
+        for (uint32_t j = 0; j < 12; j++) {
             if (j != 0) out << ",";
-            out << setw(4) << matrix[i][j];
+            out << setw(6) << matrix[i][j];
         }
         out << endl;
     }
@@ -239,17 +246,17 @@ void write_solution(const vector<vector<string>> &matrix) {
 }
 
 void output_matrix(const vector<vector<string>> &matrix) {
-    for (uint32_t i = 0; i < 10; i++) {
-        for (uint32_t j = 0; j < 10; j++)
-            cout << setw(4) << matrix[i][j] << " ";
+    for (uint32_t i = 0; i < 12; i++) {
+        for (uint32_t j = 0; j < 12; j++)
+            cout << setw(6) << matrix[i][j] << " ";
         cout << endl;
     }
 }
 
 void output_matrix(const vector<vector<uint32_t>> &matrix) {
-    for (uint32_t i = 0; i < 10; i++) {
-        for (uint32_t j = 0; j < 10; j++)
-            cout << setw(2) << ((matrix[i][j] == 0) ? "" : to_string(matrix[i][j])) << " ";
+    for (uint32_t i = 0; i < 12; i++) {
+        for (uint32_t j = 0; j < 12; j++)
+            cout << setw(6) << ((matrix[i][j] == 0) ? "" : to_string(matrix[i][j])) << " ";
         cout << endl;
     }
 }
@@ -271,9 +278,9 @@ void get_candidate_values_and_constraints(const vector<vector<string>> &matrix,
                                           vector<vector<uint32_t>> &candidates_map,
                                           vector<Constraint> &constraints_vct) {
     uint32_t cur_id = 0;
-    candidates_map.resize(10, vector<uint32_t> (10, 0));
-    for (uint32_t i = 0; i < 10; i++) {
-        for (uint32_t j = 0; j < 10; j++) {
+    candidates_map.resize(12, vector<uint32_t> (12, 0));
+    for (uint32_t i = 0; i < 12; i++) {
+        for (uint32_t j = 0; j < 12; j++) {
             auto &e = matrix[i][j];
             if (e.empty()) continue;
             if (isalpha(e.back())) continue;
@@ -281,8 +288,8 @@ void get_candidate_values_and_constraints(const vector<vector<string>> &matrix,
         }
     }
     candidate_value_coordinates.resize(cur_id + 1);
-    for (uint32_t i = 0; i < 10; i++)
-        for (uint32_t j = 0; j < 10; j++)
+    for (uint32_t i = 0; i < 12; i++)
+        for (uint32_t j = 0; j < 12; j++)
             if (candidates_map[i][j] != 0)
                 candidate_value_coordinates[candidates_map[i][j]] = make_pair(i, j);
 
@@ -291,8 +298,8 @@ void get_candidate_values_and_constraints(const vector<vector<string>> &matrix,
     direction['R'] = make_pair(0, 1);
     direction['U'] = make_pair(-1, 0);
     direction['D'] = make_pair(1, 0);
-    for (int32_t i = 0; i < 10; i++) {
-        for (int32_t j = 0; j < 10; j++) {
+    for (int32_t i = 0; i < 12; i++) {
+        for (int32_t j = 0; j < 12; j++) {
             if (matrix[i][j].empty() || !isalpha(matrix[i][j].back())) continue;
             auto e = matrix[i][j];
             int dx, dy;
@@ -302,7 +309,7 @@ void get_candidate_values_and_constraints(const vector<vector<string>> &matrix,
             constraint.sum = stoi(e);
             int x = i + dx;
             int y = j + dy;
-            while (0 <= x && x < 10 && 0 <= y && y < 10 && candidates_map[x][y] != 0) {
+            while (0 <= x && x < 12 && 0 <= y && y < 12 && candidates_map[x][y] != 0) {
                 constraint.variable_id.emplace_back(candidates_map[x][y]);
                 x += dx;
                 y += dy;
@@ -348,5 +355,11 @@ void get_dfs_order(const uint32_t number_of_candidates, const vector<Constraint>
         for (auto &p : cur_constraint.variable_id)
             p = id2index[p];
         dfs_check_point[dfs_order.size() - 1].emplace_back(cur_constraint);
+    }
+    for (uint32_t i = 1; i <= number_of_candidates; i++) {
+        if (!fixed_candidate_id.count(i))
+            dfs_order.emplace_back(i);
+        id2index[i] = dfs_order.size() - 1;
+        fixed_candidate_id.insert(i);
     }
 }
